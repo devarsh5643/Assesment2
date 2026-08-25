ff0229c5eb3d5cdfc0f9156875e3ae61713117dd
devarsh5643 <devarshpatel5643@gmail.com>
2026-08-25 18:13:40 +0930

    commit2

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => { res.locals.path = req.path; next(); });
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  res.locals.enquiryErrors = null;
+  res.locals.formData = {};
+  next();
+});
 
 // Browser forms and API submissions share the same validation contract.
 function productInputRules() {
